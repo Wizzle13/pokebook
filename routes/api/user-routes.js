a2e51aa0927f43ba2ae8656b9639d2cba3009b26
dmadd1996 <94644880+dmadd1996@users.noreply.github.com>
2022-06-08 20:47:36 -0500

    added login

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -45,6 +45,31 @@ router.post('/', (req, res) => {
         });
 });
 
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+
+        // res.json({ user: dbUserData });
+    });
+
+})
+
 router.put('/:id', (req, res) => {
     User.update(req.body, {
         individualHooks: true,
