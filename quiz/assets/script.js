e4f34ef7b4a85a3bc14f3569c8fbe2645212dab4
dmadd1996 <94644880+dmadd1996@users.noreply.github.com>
2022-06-10 19:14:16 -0500

    added questions, working on scoring system

--- quiz/assets/script.js
+++ quiz/assets/script.js
@@ -4,240 +4,158 @@ const startScreen = document.getElementById("startScreen")
 const btn = document.getElementById("btn");
 const subLine = document.getElementById("subLine")
 const quizContainer = document.getElementById('quizContainer')
-const scoreSaver = document.getElementById('scoreSaver')
-//When the game ends, this div presents the time score
-const currentScore = document.getElementById('currentScore')
-//variable to track question index
-var i = 0
-const initialsBtn = document.getElementById('initialsBtn')
-const savedScores = document.getElementById('savedScores')
-const hiddenScores = document.getElementById('hiddenScores')
+
+//question incrementor
+let i = 0
+
+//score holders
+var bug = 0
+var drag = 0
+var elct = 0
+var figt = 0
+var fire = 0
+var fly = 0
+var ghst = 0
+var grss = 0
+var grnd = 0
+var ice = 0
+var nrml = 0
+var posn = 0
+var psyc = 0
+var rock = 0
+var watr = 0
+var stel = 0
 
 
-var myQuestions = [
-  {
-    question: "Inside which HTML element do we put the JavaScript?",
+//each question will have two type IDs
+//every answer will add a point to each of these IDs
+//at the end of the quiz, if there is 1 leading type, choose that type
+//if there are 2 types tied for the lead, choose dual type pokemon
+//if there are 3+ leaders, choose normal or fighting (extra question)
 
-    answers: [
-      "js",
-      "javascript",
-      "script"
-    ],
-    correctAnswer: "script"
-  },
-  {
-    question: "To access an HTML element from JavaScript, you can use this method:",
+//answers.id[] are ordered from most to least relevant in the array
+//most relevant is 4 points, least is 1
 
-    answers: [
-      "setAttribute",
-      "getElementById",
-      "getItem"
-    ],
-    correctAnswer: "getElementById"
-  },
-  {
-    question: "How do you create a function in JavaScript?",
+var question = new Array()
 
-    answers: [
-      "function myFunction()",
-      "function = myFunction()",
-      "function.myFunction()"
-    ],
-    correctAnswer: "function myFunction()"
-  },
-  {
-    question: "Which statement tells the browser to write 'Hello Dolly' inside an HTML element with id='demo'?",
+question[0] = new Array(
+  "You are in a group, traveling on a road when Team Rocket ambushes you to steal your pokemon. What do you do?",
+  new Array(
+    "Tell everyone to spread out, while you go after the leader.",
+    new Array(
+      "Water", "Dragon", "Steel", "Fighting"
+    )
+  ),
+  new Array (
+    "Focus on the closest enemy. Once you defeat them, you'll deal with the others.",
+    new Array(
+      "Fire", "Electric", "Rock", "Flying"
+    )
+  ),
+  new Array (
+    "Use the confusion to slip out of sight. Use stealth to your advantage.",
+    new Array(
+      "Ghost", "Poison", "Psych", "Ice"
+    )
+  ),
+  new Array (
+    "Group up and work together. They can't take you all at once.",
+    new Array(
+      "Ground", "Grass", "Normal", "Bug"
+    )
+  ),
+  
+)
 
-    answers: [
-      "demo.innerHTML='Hello Dolly';",
-      "document.getElementById('demo').innerHTML='Hello Dolly';",
-      "document.getElementById('demo')= innerHTML('Hello Dolly');"
-    ],
-    correctAnswer: "document.getElementById('demo').innerHTML='Hello Dolly';"
-  },
-  {
-    question: `javascript elements are executed in what order?`,
+// var myQuestions = new Array ()
 
-    answers: [
-      "variables, functions, event listeners",
-      "functions, variables, others",
-      "in the sequence they are written"
-    ],
-    correctAnswer: "in the sequence they are written"
-  },
-  {
-    question: `used to declare a javascript variable`,
+console.log(question[i])
 
-    answers: [
-      "function",
-      "var",
-      "//"
-    ],
-    correctAnswer: "var"
-  },
-]
+// {
+//   question: "You are in a group, traveling on a road when Team Rocket ambushes you to steal your pokemon. What do you do?",
+//   index: 0,
+
+//   answers: [
+//     {
+//       message: "Tell everyone to spread out, while you go after the leader.",
+//       type: ["Water", "Dragon", "Steel", "Fighting"]
+//     },
+//     {
+//       message: "Focus on the closest enemy. Once you defeat them, you'll deal with the others.",
+//       type: ["Fire", "Electric", "Rock", "Flying"]
+//     },
+//     {
+//       message: "Group up and work together. They can't take you all at once.",
+//       type: ["Ground", "Grass", "Normal", "Bug"]
+//     },
+//     {
+//       message: "Use the confusion to slip out of sight. Use stealth to your advantage.",
+//       type: ["Ghost", "Poison", "Psych", "Ice"]
+//     },
+//   ]
+
 
 //Display quiz from array when user presses start
 function buildQuiz() {
-  quizContainer.innerHTML = `<h3>${myQuestions[i].question}</h3>
-            
-  <div class="mb-3"><button class="btn-block btn-primary" id="answerA" >${myQuestions[i].answers[0]}</button></div>
 
-  <div class="mb-3"><button class="btn-block btn-primary" id="answerB">${myQuestions[i].answers[1]}</button></div>
+  quizContainer.innerHTML = `<h3>${question[i][0]}</h3>
             
-  <div class="mb-3"><button class="btn-block btn-primary" id="answerC">${myQuestions[i].answers[2]}</button></div>`
+  <div class="mb-3"><button class="btn-block btn-primary" id="answerA" >${question[i][1][0]}</button></div>
+
+  <div class="mb-3"><button class="btn-block btn-primary" id="answerB">${question[i][2][0]}</button></div>
+            
+  <div class="mb-3"><button class="btn-block btn-primary" id="answerC">${question[i][3][0]}</button></div>
+  
+  <div class="mb-3"><button class="btn-block btn-primary" id="answerD">${question[i][4][0]}</button></div>`
 
   //variables that contain id's for each button on the screen
-  var answerA = document.getElementById('answerA')
-  var answerB = document.getElementById('answerB')
-  var answerC = document.getElementById('answerC')
+  // var answerA = document.getElementById('answerA')
+  // var answerB = document.getElementById('answerB')
+  // var answerC = document.getElementById('answerC')
+  // var answerD = document.getElementById('answerD')
 
   //each button is its own submit, and only the correct submit "id" will allow the user to increment index
   answerA.addEventListener('click', answerCheck)
   answerB.addEventListener('click', answerCheck)
   answerC.addEventListener('click', answerCheck)
+  answerD.addEventListener('click', answerCheck)
 }
 
-//timer variables
-let time = myQuestions.length * 20;
-let timer;
-const timerEl = document.getElementById('timerEl')
 
-//timer function
-function handleTimer() {
-  //timer counts down
-  time--;
-  timerEl.textContent = time;
-
-  if (time <= 0) {
-    endGame()
-  }
-}
-
-//colors red for 2 seconds on incorrect answer
-function highlight() {
-
-  timerEl.setAttribute('class', 'text-danger font-weight-bold');
-
-  setTimeout(function () {
-    timerEl.removeAttribute('class', 'text-danger font-weight-bold');
-  }, 2000);
-
-  var answerA = document.getElementById('answerA')
-  var answerB = document.getElementById('answerB')
-  var answerC = document.getElementById('answerC')
-
-  answerA.setAttribute('class', 'btn-block btn-danger');
-
-  setTimeout(function () {
-    answerA.setAttribute('class', 'btn-block btn-primary');
-  }, 2000);
-
-  answerB.setAttribute('class', 'btn-block btn-danger');
-
-  setTimeout(function () {
-    answerB.setAttribute('class', 'btn-block btn-primary');
-  }, 2000);
-
-  answerC.setAttribute('class', 'btn-block btn-danger');
-
-  setTimeout(function () {
-    answerC.setAttribute('class', 'btn-block btn-primary');
-  }, 2000);
-}
 
 function answerCheck() {
-  //console.log(this.innerText)
 
-
-  if (this.innerText === myQuestions[i].correctAnswer) {
-    //if answer is correct
-    console.log('correct!')
-    //increment the index
-    i++
-    console.log(`index is ${i}`)
-    console.log(`myQuestions length is ${myQuestions.length}`)
-    //if the quiz is over
-    if (myQuestions.length == i) {
-      //end the quiz
-      endGame()
-    } else {
-      //rebuild the quiz question
-      buildQuiz()
-    }
-  } else {
-    console.log('false!')
-    //deduct time
-    time = time - 15
-
-    highlight()
+  if (this.id === "answerA") {
+    question[i][1][1][0]+=4
+    question[i][1][1][1]+=3
+    question[i][1][1][2]+=2
+    question[i][1][1][3]+=1
+    console.log([bug,drag,elct,figt,fire,fly,ghst,grss,grnd,ice,nrml,posn,psyc,rock,watr,stel])
+  } else if (this.id === "answerB") {
+    answerScoreB++
+  } else if (this.id === "answerC") {
+    answerScoreC++
+  } else if (this.id === "answerD") {
+    answerScoreD++
   }
+  
+  i++;
+  buildQuiz()
+  
+  // } else if (answerScoreA == 4) {
+  //   callResult("type/3")
+  // } else if (answerScoreB == 4) {
+  //   callResult(answerScoreB)
+  // } else if (answerScoreC == 4) {
+  //   callResult(answerScoreC)
+  // } else {
+  //   callResult(answerScoreD)
+  // }
 }
 
-function endGame() {
-  //hide timer
-  timerEl.setAttribute('class', 'hidden')
-
-  //set time to zero and clear interval
-  clearInterval(timer)
-
-  //ask the user if they would like to proceed to end of game, score save
-  if (window.confirm("GAME OVER: Would you like to save your score?")) {
-    highScore()
-    //if not, reload
-  } else {
-    location.reload()
-  }
-
+function callResult(hello) {
+  console.log(hello)
 }
-
-function highScore() {
-  //hide quiz
-  quizContainer.setAttribute('class', 'hidden')
-  //reveal end of game screen
-  scoreSaver.removeAttribute('class', 'hidden')
-  //add HTML to id=currentScore to display time score
-  currentScore.innerHTML = `Your score is ${time}`
-}
-
-// function displayScores() {
-//   savedScores.textContent = displayedScoreArray
-// }
-
-function printHighscores() {
-  //pull from local
-  var highScoresParse = JSON.parse(localStorage.getItem('highScores') || [])
-  console.log("hs", JSON.stringify(highScoresParse))
-  // var highScoresString = JSON.stringify(highScoresParse)
-
-  for (let i = 0; i < highScoresParse.length; i++) {
-    savedScores.innerHTML += `<p> ${highScoresParse[i].initials} scored ${highScoresParse[i].score} points </p>`;
-  }
-}
-
-var initials = document.getElementById('initials')
-var scoreArray = JSON.parse(window.localStorage.getItem("highScores")) || [];
-
-initialsBtn.onclick = function scoreSave() {
-
-  //compile user input and remaining time into one var
-  var newScore = {
-    'initials': initials.value,
-    'score': time
-  }
-
-  //push to array
-  scoreArray.push(newScore)
-
-  //push array to local (see printHighScores() for pull)
-  localStorage.setItem('highScores', JSON.stringify(scoreArray))
-
-  //reload page on submit
-  location.reload()
-
-}
-
-// savedScores.textContent = JSON.parse(localStorage.getItem('highScores'))
 
 //when user clicks start, the timer starts and quiz is built
 btn.onclick = function hideStart() {
@@ -245,12 +163,9 @@ btn.onclick = function hideStart() {
   subLine.setAttribute("class", "hidden")
   quizContainer.removeAttribute('class', 'hidden')
 
-  timer = setInterval(handleTimer, 1000);
-  timerEl.textContent = time;
-
   buildQuiz()
 }
-printHighscores()
+
 
 //     https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/submit_event
 //     plus you need https://developer.mozilla.org/en-US/docs/Web/API/FormData for getting the FormData
