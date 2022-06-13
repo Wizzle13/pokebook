b76018a2860a25b2f4224589a369f1f6a2852840
dmadd1996 <94644880+dmadd1996@users.noreply.github.com>
2022-06-13 01:42:01 -0500

    finished quiz, added pokemon and api calls

--- quiz/assets/script.js
+++ quiz/assets/script.js
@@ -132,22 +132,155 @@ question[0] = new Array(
       )
     ),
     new Array(
-      "One that shares my personal goals is best.",
+      "Take time to understand the problem. The solution will show itself.",
       new Array(
-        elct, rock, ghst, fire
+        psyc, posn, ghst, bug
       )
     ),
     new Array(
-      "Whichever companion that will help me win matches.",
+      "Deploy all my powerful pokemon, they will get the job done.",
       new Array(
-        figt, ice, grnd, posn
+        fly, elct, fire, ice
       )
     )
   ),
+  question[4] = new Array(
+    "Favorite vacation spot.",
+    new Array(
+      "Anywhere remote; somewhere I have never been.",
+      new Array(
+        drag, elct, fly, psyc
+      )
+    ),
+    new Array(
+      "It has to be warm, with an ocean view",
+      new Array(
+        fire, nrml, watr, bug
+      )
+    ),
+    new Array(
+      "Somewhere harsh but beautiful.",
+      new Array(
+        psyc, posn, ghst, bug
+      )
+    ),
+    new Array(
+      "I don't need anything special. Something close to home is best.",
+      new Array(
+        rock, grss, ghst, figt
+      )
+    )
+  ),
+  question[5] = new Array(
+    "What is your favorite thing to do on a friday night?",
+    new Array(
+      "Play video games.",
+      new Array(
+        stel, nrml, elct, grnd
+      )
+    ),
+    new Array(
+      "Go out to a restaurant.",
+      new Array(
+        drag, grss, watr, posn
+      )
+    ),
+    new Array(
+      "Go to the movies.",
+      new Array(
+        psyc, figt, fire, fly
+      )
+    ),
+    new Array(
+      "Stay in and read a book.",
+      new Array(
+        bug, ghst, rock, ice
+      )
+    )
+  ),
+  question[6] = new Array(
+    "You are losing a pokemon battle to a superior team. How do you react?",
+    new Array(
+      "My pokemon need to learn how to handle themselves while at a disadvantage. Let them lose.",
+      new Array(
+        figt, stel, rock, grnd
+      )
+    ),
+    new Array(
+      "The battle is never over. Use items and go on the offensive.",
+      new Array(
+        drag, fire, ice, nrml
+      )
+    ),
+    new Array(
+      "Surrender the match. My pokemon do not deserve this treatment.",
+      new Array(
+        fly, grss, posn, psyc
+      )
+    ),
+    new Array(
+      "Stick to defensive abilities and get the team's stats up while we wait for an opening.",
+      new Array(
+        bug, ghst, watr, elct
+      )
+    )
+  ),
+  question[6] = new Array(
+    "You are losing a pokemon battle to a superior team. How do you react?",
+    new Array(
+      "My pokemon need to learn how to handle themselves while at a disadvantage. Let them lose.",
+      new Array(
+        figt, stel, rock, grnd
+      )
+    ),
+    new Array(
+      "The battle is never over. Use items and go on the offensive.",
+      new Array(
+        drag, fire, ice, nrml
+      )
+    ),
+    new Array(
+      "Surrender the match. My pokemon do not deserve this treatment.",
+      new Array(
+        fly, grss, posn, psyc
+      )
+    ),
+    new Array(
+      "Stick to defensive abilities and get the team's stats up while we wait for an opening.",
+      new Array(
+        bug, ghst, watr, elct
+      )
+    )
+  ),
+  question[7] = new Array(
+    "If you could have one super power, what would it be?",
+    new Array(
+      "Super strength",
+      new Array(
+        figt, nrml, stel, rock
+      )
+    ),
+    new Array(
+      "Flight",
+      new Array(
+        fly, drag, ghst, bug
+      )
+    ),
+    new Array(
+      "Mind Reading",
+      new Array(
+        posn, ice, watr, psyc
+      )
+    ),
+    new Array(
+      "Ability to create life",
+      new Array(
+        fire, grss, grnd, elct
+      )
+    )
+  )
 
-  // var myQuestions = new Array ()
-
-  console.log(question[i])
+// console.log(question[i])
 
 //Display quiz from array when user presses start
 function buildQuiz() {
@@ -179,26 +312,30 @@ function answerCheck() {
 
   if (this.id === "answerA") {
     userChoices = userChoices.concat(question[i][1][1])
-    console.log(question[i][1][1])
-    console.log(userChoices)
+    // console.log(question[i][1][1])
+    // console.log(userChoices)
   } else if (this.id === "answerB") {
     userChoices = userChoices.concat(question[i][2][1])
-    console.log(question[i][2][1])
-    console.log(userChoices)
+    // console.log(question[i][2][1])
+    // console.log(userChoices)
   } else if (this.id === "answerC") {
     userChoices = userChoices.concat(question[i][3][1])
-    console.log(question[i][3][1])
-    console.log(userChoices)
+    // console.log(question[i][3][1])
+    // console.log(userChoices)
   } else if (this.id === "answerD") {
     userChoices = userChoices.concat(question[i][4][1])
-    console.log(question[i][4][1])
-    console.log(userChoices)
+    // console.log(question[i][4][1])
+    // console.log(userChoices)
   };
-
-  findMax(userChoices)
-
-  i++;
-  buildQuiz()
+  console.log(i)
+  console.log(question.length)
+  // findMax(userChoices)
+  if (i < 7) {
+    i++;
+    buildQuiz()
+  } else {
+    findMax(userChoices)
+  }
 }
 
 function findMax(arr1) {
@@ -217,7 +354,167 @@ function findMax(arr1) {
     m = 0;
   }
   console.log(item + " ( " + mf + " times ) ");
+
+  showResults(item)
 }
+
+function showResults(item) {
+  const endScreen = document.getElementById('endScreen')
+  const pokemonType = document.getElementById('pokemonType')
+
+  quizContainer.setAttribute('class', 'hidden')
+  endScreen.removeAttribute('class', 'hidden')
+
+  if (item == 1) {
+    pokemonType.innerText = " Bug"
+    var choice = [
+      "scyther",
+      "beedrill",
+      "caterpie"
+    ]
+  } else if (item == 2) {
+    pokemonType.innerText = " Dragon"
+    var choice = [
+      "dragonair",
+      "dragonite",
+      "gible"
+    ]
+  } else if (item == 3) {
+    pokemonType.innerText = " Electric"
+    var choice = [
+      "pikachu",
+      "electabuzz",
+      "jolteon"
+    ]
+  } else if (item == 4) {
+    pokemonType.innerText = " Fighting"
+    var choice = [
+      "machoke",
+      "hitmonlee",
+      "hitmonchan"
+    ]
+  } else if (item == 5) {
+    pokemonType.innerText = " Fire"
+    var choice = [
+      "ninetales",
+      "charizard",
+      "cyndaquil"
+    ]
+  } else if (item == 6) {
+    pokemonType.innerText = " Flying"
+    var choice = [
+      "pidgeotto",
+      "farfetchd",
+      "mantine"
+    ]
+  } else if (item == 7) {
+    pokemonType.innerText = " Ghost"
+    var choice = [
+      "banette",
+      "gengar",
+      "drifloon"
+    ]
+  } else if (item == 8) {
+    pokemonType.innerText = " Grass"
+    var choice = [
+      "ivysaur",
+      "chikorita",
+      "bellossom"
+    ]
+  } else if (item == 9) {
+    pokemonType.innerText = " Ground"
+    var choice = [
+      "cubone",
+      "sandslash",
+      "rhyhorn"
+    ]
+  } else if (item == 10) {
+    pokemonType.innerText = " Ice"
+    var choice = [
+      "swinub",
+      "regice",
+      "walrein"
+    ]
+  } else if (item == 11) {
+    pokemonType.innerText = " Normal"
+    var choice = [
+      "meowth",
+      "kangaskhan",
+      "snorlax"
+    ]
+  } else if (item == 12) {
+    pokemonType.innerText = " Poison"
+    var choice = [
+      "arbok",
+      "nidoqueen",
+      "muk"
+    ]
+  } else if (item == 13) {
+    pokemonType.innerText = " Psychic"
+    var choice = [
+      "abra",
+      "wobbuffet",
+      "mewtwo"
+    ]
+  } else if (item == 14) {
+    pokemonType.innerText = " Rock"
+    var choice = [
+      "geodude",
+      "onix",
+      "aerodactyl"
+    ]
+  } else if (item == 15) {
+    pokemonType.innerText = " Water"
+    var choice = [
+      "squirtle",
+      "gyarados",
+      "starmie"
+    ]
+  } else if (item == 16) {
+    pokemonType.innerText = " Steel"
+    var choice = [
+      "steelix",
+      "aggron",
+      "jirachi"
+    ]
+  }
+
+  fetchPokemon(choice)
+}
+
+function fetchPokemon(choice) {
+  const pokemonSprite1 = document.getElementById('pokemonSprite1')
+  const pokemonSprite2 = document.getElementById('pokemonSprite2')
+  const pokemonSprite3 = document.getElementById('pokemonSprite3')
+
+  var pokeAPI1 = `https://pokeapi.co/api/v2/pokemon/${choice[0]}`
+  var pokeAPI2 = `https://pokeapi.co/api/v2/pokemon/${choice[1]}`
+  var pokeAPI3 = `https://pokeapi.co/api/v2/pokemon/${choice[2]}`
+
+  fetch(pokeAPI1).then(function (response) {
+    response.json().then(function (pokeInfo1) {
+      console.log(pokeInfo1.id);
+
+      pokemonSprite1.innerHTML = `<img src="${pokeInfo1.sprites.front_default}" alt="pokemon1">`
+    })
+  })
+  fetch(pokeAPI2).then(function (response) {
+    response.json().then(function (pokeInfo2) {
+      console.log(pokeInfo2.id);
+
+      pokemonSprite2.innerHTML = `<img src="${pokeInfo2.sprites.front_default}" alt="pokemon2">`
+    })
+  })
+  fetch(pokeAPI3).then(function (response) {
+    response.json().then(function (pokeInfo3) {
+      console.log(pokeInfo3.id);
+
+      pokemonSprite3.innerHTML = `<img src="${pokeInfo3.sprites.front_default}" alt="pokemon3">`
+    })
+  })
+}
+// pokemonSprite.innerHTML = `<img src="${fetch1.sprites}" alt="pokemon1"> <img src="${fetch2.sprites}" alt="pokemon2"> <img src="${fetch3.sprites}" alt="pokemon3">`
+
 
 
 btn.onclick = function hideStart() {
@@ -227,7 +524,3 @@ btn.onclick = function hideStart() {
 
   buildQuiz()
 }
-
-
-//     https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/submit_event
-//     plus you need https://developer.mozilla.org/en-US/docs/Web/API/FormData for getting the FormData
